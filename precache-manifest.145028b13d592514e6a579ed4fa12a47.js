be68ab721b60ba70efe8e86c2a4a1ab31b6b93c6
AmirAllami <allamiamir@gmail.com>
2020-08-22 23:19:02 +0100

    Updates

---
+++ precache-manifest.145028b13d592514e6a579ed4fa12a47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9f1513f64b974e2771829a17da22ef",
+    "url": "/color-project/index.html"
+  },
+  {
+    "revision": "250855c22d748832824f",
+    "url": "/color-project/static/css/2.2a4460b4.chunk.css"
+  },
+  {
+    "revision": "197c172ad5e59b1f3ab7",
+    "url": "/color-project/static/css/main.a78dc8fa.chunk.css"
+  },
+  {
+    "revision": "250855c22d748832824f",
+    "url": "/color-project/static/js/2.93ae54f3.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/color-project/static/js/2.93ae54f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197c172ad5e59b1f3ab7",
+    "url": "/color-project/static/js/main.079f589d.chunk.js"
+  },
+  {
+    "revision": "b2c2df169e498fcff304",
+    "url": "/color-project/static/js/runtime-main.09d4c853.js"
+  },
+  {
+    "revision": "221da266a9d82e4f0cc7213e06ec7514",
+    "url": "/color-project/static/media/bg.221da266.svg"
+  }
+]);
